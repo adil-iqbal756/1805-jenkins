6f8a5e9f2bfddf7162ec964591cce00ed01b8848
adil iqbal <iqad009@gmail.com>
2018-06-26 14:40:02 -0400

    blueball

---
+++ SimpleProject/src/test/java/Tests.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class Tests {
+@Test
+public void test() {
+	assertEquals("should return true",true,(new TestClass().test()));
+}
+}
